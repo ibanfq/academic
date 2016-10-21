b454ef917a22386eed6d19e1de6dac30425de0f6
Iban Fuentes <ibanfuentes@gmail.com>
2016-10-21 11:50:53 +0100

    Fix bookings_controller authorization

--- app/controllers/app_controller.php
+++ app/controllers/app_controller.php
@@ -62,7 +62,7 @@ class AppController extends Controller {
       $this->Auth->allow('login');
       $this->Auth->allow('rememberPassword', 'calendar_by_classroom', 'calendar_by_subject', 'calendar_by_level', 'board', 'get', 'get_by_level', 'find_subjects_by_name', 'get_by_subject', 'add_by_secret_code', 'clean_up_day');
 
-      if ($this->params['controller'] == 'events') {
+      if ($this->params['controller'] == 'events' || $this->params['controller'] == 'bookings') {
         $this->Auth->allow('view');
       }
 
