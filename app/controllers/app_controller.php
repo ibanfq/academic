c0c8db7e935fd63b7949736154b0653a6613778b
Iban Fuentes <ibanfuentes@gmail.com>
2019-09-25 20:50:27 +0100

    app_controller improvements

--- app/controllers/app_controller.php
+++ app/controllers/app_controller.php
@@ -29,11 +29,16 @@ class AppController extends Controller {
 
   function beforeRender() {
     $this->webroot = '/';
+    $this->set("acl", $this->Acl);
+    $this->set("auth", $this->Auth->user('id') ? $this->Auth : null);
   }
 
 	function beforeFilter() {
     $this->layout = 'default';
 
+    /**
+     * Enable debug email if debug_email setted to true
+     */
     if (Configure::read('debug_email')) {
       $this->Email->delivery = 'debug';
       $this->Email = new ObjectProxy($this->Email);
@@ -47,10 +52,28 @@ class AppController extends Controller {
       });
     }
 
+    /**
+     * Configure session path (override the router base url)
+     */
     $this->Session->path = '/';
 
+    /**
+     * Disable csrf validation.
+     *
+     * @todo enable csrf security except to api requests
+     */
+    $this->Security->validatePost = false;
+    
+    /**
+     * Initialize the environment lib
+     */
     $this->_initEnvironment();
 
+    /**
+     * Change login scope per institution login form
+     *
+     * @deprecated in favour of CAS login
+     */
     if (Environment::institution('id')) {
       $authModel = ClassRegistry::init($this->Auth->userModel);
       $db = $authModel->getDataSource();
@@ -59,60 +82,19 @@ class AppController extends Controller {
       );
     }
 
-    $this->_updateAppBetaOptions();
+    /**
+     * Set HTML authorization
+     */
+    if (!$this->isApi) {
+      // Check logged user beta options
+      $this->_updateAppBetaOptions();
 
-    $this->Security->validatePost = false;
-    
-    if ($this->isApi) {
-      // Read Authorization header
-      $authorization = env('HTTP_AUTHORIZATION');
-      if (empty($authorization)) {
-        $authorization = env('REDIRECT_HTTP_AUTHORIZATION');
-      }
-
-      if (($authorization || env('PHP_AUTH_USER')) && !$this->Session->started()) {
-        // Fake php sessions
-        session_set_save_handler(
-          array(__CLASS__, '__fakeSessionWrite'),
-          array(__CLASS__, '__fakeSessionWrite'),
-          array(__CLASS__, '__fakeSessionWrite'),
-          array(__CLASS__, '__fakeSessionWrite'),
-          array(__CLASS__, '__fakeSessionWrite'),
-          array(__CLASS__, '__fakeSessionWrite')
-        );
-        ini_set('session.use_cookies', 0);
-        session_start();
-      }
-      
-      // Set login options
-      $this->Security->loginOptions = array(
-        'type' => 'basic',
-        'realm' => 'academic',
-        'login' => '_api_authenticate'
-      );
-      
-      // Authorize
-      if ($authorization || !$this->_authorize()) {
-        if (!env('PHP_AUTH_USER') && preg_match('/(Basic|Bearer)\s+(.*)$/i', $authorization, $matches)) {
-          if (strtolower($matches[1]) === 'basic') {
-            $name_and_password = explode(':', base64_decode($matches[2]));
-            $name = $name_and_password[0];
-            $password = isset($name_and_password[1])? $name_and_password[1] : '';
-            $_SERVER['PHP_AUTH_USER'] = strip_tags($name);
-            $_SERVER['PHP_AUTH_PW'] = strip_tags($password);
-          } else {
-            $_SERVER['PHP_AUTH_USER'] = $matches[2];
-            $_SERVER['PHP_AUTH_PWD'] = null;
-          }
-        }
-        $this->Security->requireLogin();
-      } else {
-        $this->Auth->allow($this->params['action']);
-      }
-    } else {
+      // Set auth options
       $this->Auth->loginAction = array('controller' => 'users', 'action' => 'login', 'base' => false);
       $this->Auth->logoutRedirect = Router::url(array('controller' => 'users', 'action' => 'login', 'base' => false, 'full_base' => true));
       $this->Auth->loginRedirect = Router::url(array('controller' => 'users', 'action' => 'home', 'base' => false, 'full_base' => true));
+
+      // Allow public actions
       $this->Auth->allow('login');
       $this->Auth->allow('rememberPassword', 'find_subjects_by_name', 'add_by_secret_code', 'clean_up_day');
 
@@ -120,13 +102,18 @@ class AppController extends Controller {
         $this->Auth->allow('view', 'get');
       }
 
+      // Check acl authorization and 
       $auth_type = $this->Auth->user('type');
       $acl = Configure::read('app.acl');
+
       if ($auth_type && !empty($acl[$auth_type]["{$this->params['controller']}.{$this->params['action']}"])) {
+        // Authorize by user type acl options
         $this->Auth->allow($this->params['action']);
       } elseif (!empty($acl['all']["{$this->params['controller']}.{$this->params['action']}"])) {
+        // Authorize by global acl options
         $this->Auth->allow($this->params['action']);
       } elseif (!$this->_authorize()) {
+        // Fail to authorize
         if ($this->RequestHandler->isAjax()) {
           $this->redirect(null, 401, true);
         } elseif ($this->Auth->user('id') == null) {
@@ -143,11 +130,76 @@ class AppController extends Controller {
       }
     }
 
-    if (! Environment::user('id')) {
-      Environment::setUser($this->Auth->user());
+    /**
+     * Set API authorization
+     */
+    if ($this->isApi) {
+      // Read Authorization header
+      $authorization = env('HTTP_AUTHORIZATION');
+      if (empty($authorization)) {
+        $authorization = env('REDIRECT_HTTP_AUTHORIZATION');
+      }
+
+      if (!$authorization) {
+        // Check logged user beta options
+        $this->_updateAppBetaOptions();
+      }
+
+      if (($authorization || env('PHP_AUTH_USER')) && !$this->Session->started()) {
+        // Fake php sessions to add api login compatibility to PHPCake
+        session_set_save_handler(
+          array(__CLASS__, '__fakeSessionWrite'),
+          array(__CLASS__, '__fakeSessionWrite'),
+          array(__CLASS__, '__fakeSessionWrite'),
+          array(__CLASS__, '__fakeSessionWrite'),
+          array(__CLASS__, '__fakeSessionWrite'),
+          array(__CLASS__, '__fakeSessionWrite')
+        );
+        ini_set('session.use_cookies', 0);
+        session_start();
+      }
+      
+      // Set login options to api authenticate
+      $this->Security->loginOptions = array(
+        'type' => 'basic',
+        'realm' => 'academic',
+        'login' => '_api_authenticate'
+      );
+      
+      // Authorize
+      if ($authorization || !$this->_authorize()) {
+        // Call to api login method if current request has authorization header or if controller denied the access
+        if (!env('PHP_AUTH_USER') && preg_match('/(Basic|Bearer)\s+(.*)$/i', $authorization, $matches)) {
+          if (strtolower($matches[1]) === 'basic') {
+            /**
+             * Pass Basic authorization to api login method
+             * 
+             * @deprecated in favour of CAS login
+             */
+            $name_and_password = explode(':', base64_decode($matches[2]));
+            $name = $name_and_password[0];
+            $password = isset($name_and_password[1])? $name_and_password[1] : '';
+            $_SERVER['PHP_AUTH_USER'] = strip_tags($name);
+            $_SERVER['PHP_AUTH_PW'] = strip_tags($password);
+          } else {
+            // Pass Bearer authorization to api login method
+            $_SERVER['PHP_AUTH_USER'] = $matches[2];
+            $_SERVER['PHP_AUTH_PWD'] = null;
+          }
+        }
+
+        // Force to call api login action in startup step
+        $this->Security->requireLogin();
+      } else {
+        // if ( !$authorization && $this->_authorize() ) then allow user to call the action
+        $this->Auth->allow($this->params['action']);
+      }
     }
 
-    if (! $this->Auth->user('super_admin') && Environment::institution('id') && ! Environment::userInstitution('active')) {
+    /**
+     * If user is logged then check if has access to current institution.
+     */
+    if (Environment::institution('id') && $this->Auth->user('id') && ! $this->Auth->user('super_admin') && ! Environment::userInstitution('active')) {
       if ($this->RequestHandler->isAjax()) {
         $this->redirect(null, 401, true);
       } else {
@@ -186,6 +238,7 @@ class AppController extends Controller {
       }
     }
 
+    // Check user beta options
     $this->_updateAppBetaOptions();
   }
 
@@ -259,6 +312,8 @@ class AppController extends Controller {
     } else {
         Configure::load('app');
     }
+
+    Environment::setUser($this->Auth->user());
   }
 
   function _updateAppBetaOptions()
@@ -273,14 +328,11 @@ class AppController extends Controller {
     }
   }
 
-	function _authorize() {
-    $this->set("acl", $this->Acl);
-    
+	function _authorize() {    
 		if ($this->Auth->user('id') != null) {
-      $this->set("auth", $this->Auth);
-      
       return true;
-		}
+    }
+    
     return false;
 	}
 
