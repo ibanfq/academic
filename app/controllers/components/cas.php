2a3a274eda610675c66448109034b29bd0780903
Iban Fuentes <ibanfuentes@gmail.com>
2019-09-26 20:54:27 +0100

    Add cas configuration to config/core

--- app/controllers/components/cas.php
+++ app/controllers/components/cas.php
@@ -26,12 +26,20 @@ class CasComponent extends Object {
         }
 
         // Initialize phpCAS
-        phpCAS::client(CAS_VERSION_2_0, 'identificate.ulpgc.es', 443, '/cas');
+        phpCAS::client(
+            constant(Configure::read('CAS.version')),
+            Configure::read('CAS.hostname'),
+            Configure::read('CAS.port'),
+            Configure::read('CAS.context')
+        );
 
-        if ($debug) {
+        $cas_server_ca_cert_path = Configure::read('CAS.server_ca_cert_path');
+
+        if ($cas_server_ca_cert_path) {
             // For production use set the CA certificate that is the issuer of the cert
             // on the CAS server and uncomment the line below
-            // phpCAS::setCasServerCACert($cas_server_ca_cert_path);
+            phpCAS::setCasServerCACert($cas_server_ca_cert_path);
+        } else {
             // For quick testing you can disable SSL validation of the CAS server.
             // THIS SETTING IS NOT RECOMMENDED FOR PRODUCTION.
             // VALIDATING THE CAS SERVER IS CRUCIAL TO THE SECURITY OF THE CAS PROTOCOL!
