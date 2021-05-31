ae2e6aced74497f4cc5f80d942ebeaf820476269
Iban Fuentes <ibanfuentes@gmail.com>
2021-05-31 19:16:37 +0100

    Check if user is super_admin in changeuser admin tool

--- app/webroot/admin-tools/changeuser.php
+++ app/webroot/admin-tools/changeuser.php
@@ -15,7 +15,7 @@ $session->start();
 
 if ($session->check('Auth.User')) {
     $user = $session->read('Auth.User');
-    if ($user['type'] === 'Administrador' && $user['id'] !== $_GET['id']) {
+    if ($user['type'] === 'Administrador' && $user['super_admin'] && $user['id'] !== $_GET['id']) {
         $userModel = ClassRegistry::init('User');
         $newUser = $userModel->findById($_GET['id']);
         if ($newUser) {
